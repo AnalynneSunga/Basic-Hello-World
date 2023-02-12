2e3690a4432bc5f38a36821717b9881f5d45e4b2
Analynne Sunga <112285768+AnalynneSunga@users.noreply.github.com>
2023-02-12 06:26:14 +0000

    Delete hello world

--- .ccls-cache/@home@runner@Basic-Hello-World/main.cpp
+++ .ccls-cache/@home@runner@Basic-Hello-World/main.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 int main() {
-  cout << "Hello world" << endl;
   cout << "My name is ******** *****" << endl;
   cout << "I am ** years old." << endl;
   cout << "I enjoy ********" << endl;
